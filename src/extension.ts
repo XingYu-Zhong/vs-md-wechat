2d5580400259b64b7b8642c11aa0fb3f767bae25
XFS-小风 <xiaofengs@yeah.net>
2024-08-01 10:10:23 +0800

    Complete

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,6 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "webview-sample" is now active!');
-
     let disposable = vscode.commands.registerCommand('extension.showWebview', () => {
         const panel = vscode.window.createWebviewPanel(
             'vs-md-wechat-new', // Identifies the type of the webview. Used internally
@@ -12,18 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.ViewColumn.One, // Editor column to show the new webview panel in
             {
                 enableScripts: true, // Enable JavaScript in the webview
-                retainContextWhenHidden: true, //
             }
         );
-        panel.webview.html = getHtmlContentForWebview(context, panel.webview);
+        let htmlContent:string = getHtmlContentForWebview(context, panel.webview);
+        console.log(htmlContent);
+        panel.webview.html = htmlContent;
+
     });
 
     context.subscriptions.push(disposable);
-
-    const provider = new SidebarProvider(context);
-    context.subscriptions.push(
-        vscode.window.registerWebviewViewProvider('vsMdView', provider)
-    );
 }
 
 
@@ -31,10 +26,8 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {}
 
 function getHtmlContentForWebview(context: vscode.ExtensionContext, webview: vscode.Webview): string {
-    const htmlPath: string = path.join(context.extensionPath, 'src', 'md', 'index.html');
+    const htmlPath: string = path.join(context.extensionPath,'resources' ,'md', 'index.html');
         const htmlContent: string = fs.readFileSync(htmlPath, 'utf8');
-        console.log(htmlPath);
-        console.log(htmlContent);
         // Update resource paths in the HTML content
         const updatedHtmlContent = updateHtmlContentForWebview(htmlContent, webview, context.extensionPath);
         return updatedHtmlContent;
@@ -48,26 +41,8 @@ function updateHtmlContentForWebview(htmlContent: string, webview: vscode.Webvie
         if (p2.startsWith('http')) {
             return `${p1}="${p2}"`;
         }
-        const resourcePath = vscode.Uri.file(path.join(extensionPath, 'src', 'md', p2));
+        const resourcePath = vscode.Uri.file(path.join(extensionPath, 'resources', 'md', p2));
         const webviewUri = webview.asWebviewUri(resourcePath);
         return `${p1}="${webviewUri}"`;
     });
-}
-
-
-class SidebarProvider implements vscode.WebviewViewProvider {
-    context: vscode.ExtensionContext;
-    constructor(context: vscode.ExtensionContext) {
-    this.context = context;
-  }
-    // 实现 resolveWebviewView 方法，用于处理 WebviewView 的创建和设置
-  resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
-    // 配置 WebviewView 的选项
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [this.context.extensionUri]
-    };
-    // 设置 WebviewView 的 HTML 内容，可以在这里指定要加载的网页内容
-    webviewView.webview.html = getHtmlContentForWebview(this.context, webviewView.webview);
-}
 }
